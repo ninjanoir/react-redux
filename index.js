00e82c0fcccaa7a0ad8ebe7b4558296ddb862f30
Alain Robinson <40420986+ninjanoir@users.noreply.github.com>
2020-04-16 21:57:35 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-import { combineReducers } from "redux";
-import postsReducer from "./postsReducer";
-import usersReducer from "./usersReducer";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import App from "./components/App";
+import reducers from "./reducers";
 
-export default combineReducers({
-  posts: postsReducer,
-  users: usersReducer,
-});
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
